5aa891aa583e3534ae8f857fea20554e4e3e82bb
nuelsoft <nuel.mailbox@gmail.com>
2020-01-06 10:31:29 +0400

    implemented raffle modal

--- script/c.js
+++ script/c.js
@@ -1,7 +1,8 @@
+let x, y, z = 0;
 let t = setInterval(function () {
-    let x = Math.floor(Math.random() * 10);
-    let y = Math.floor(Math.random() * 10);
-    let z = Math.floor(Math.random() * 10);
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * 10);
+    z = Math.floor(Math.random() * 10);
 
     $('#nid-1').text(x.toString());
     $('#nid-2').text(y.toString());
@@ -13,5 +14,30 @@ let ra = $('#raffle');
 
 ra.on('click', async () => {
     clearInterval(t);
+    // ra.modal('show');
+    $('#raffle-val').text('' + x + y + z);
 
+});
+
+$('#raffle-close').on('click', () => {
+    t = setInterval(function () {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        z = Math.floor(Math.random() * 10);
+
+        $('#nid-1').text(x.toString());
+        $('#nid-2').text(y.toString());
+        $('#nid-3').text(z.toString());
+    }, 100);
+});
+$('#draw-again').on('click', () => {
+    t = setInterval(function () {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        z = Math.floor(Math.random() * 10);
+
+        $('#nid-1').text(x.toString());
+        $('#nid-2').text(y.toString());
+        $('#nid-3').text(z.toString());
+    }, 100);
 });
