4f2ba71b1aa4a419b7743652866e89df9a4b6ee0
nuelsoft <nuel.mailbox@gmail.com>
2020-01-08 22:20:03 +0400

    just another commit

--- script/c.js
+++ script/c.js
@@ -42,3 +42,46 @@ $('#draw-again').on('click', () => {
         clearInterval(c)
     }, 600);
 });
+
+const API_publicKey = "FLWPUBK_TEST-4be08a2d552a7dcd0056882cacc767d3-X";
+
+function payWithRave() {
+
+    console.log('called');
+
+    let phone = $('#phone');
+    let v = $('#inbox').val();
+    let email = $('#email').val();
+
+    var x = getpaidSetup({
+        PBFPubKey: API_publicKey,
+        customer_email: email,
+        amount: 1000,
+        customer_phone: phone.val(),
+        currency: "NGN",
+        redirect_url: "https:api-raffleit.herokuapp.com/api/draw",
+        txref: $('#name').val() + ':' + phone.val() + ':' + email + ':' + v + ':' + Date.now(),
+        hosted_payment: 1,
+        meta: [{
+            metaname: "raffle-value",
+            metavalue: v
+        }],
+
+        // callback: function (response) {
+        //     var txref = response.tx.txRef; // collect txRef returned and pass to a erver page to complete status check.
+        //     console.log("This is the response returned after a charge", response);
+        //     if (
+        //         response.tx.chargeResponseCode == "00" ||
+        //         response.tx.chargeResponseCode == "0"
+        //     ) {
+        //         console.log('succeeded')
+        //     } else {
+        //         console.log('failed')
+        //     }
+        //
+        //
+        //     x.close(); // use this to close the modal immediately after payment.
+        // }
+    });
+
+}
